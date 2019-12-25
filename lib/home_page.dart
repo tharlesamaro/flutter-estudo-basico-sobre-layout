1d1be2dd16bfdf5a873e23f32985f5b4c1a27f29
Tharles Amaro <tharlesamaro@gmail.com>
2019-12-25 14:17:56 -0300

    Inserindo componente PageView

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -19,7 +19,22 @@ class HomePage extends StatelessWidget {
       color: Colors.white,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[_text(), _img(), _buttons()],
+        children: <Widget>[_text(), _pageView(), _buttons()],
+      ),
+    );
+  }
+
+  Container _pageView() {
+    return Container(
+      height: 300,
+      child: PageView(
+        children: <Widget>[
+          _img("assets/images/dog1.png"),
+          _img("assets/images/dog2.png"),
+          _img("assets/images/dog3.png"),
+          _img("assets/images/dog4.png"),
+          _img("assets/images/dog5.png"),
+        ],
       ),
     );
   }
@@ -40,7 +55,7 @@ class HomePage extends StatelessWidget {
           children: <Widget>[
             _button("Snack"),
             _button("Dialog"),
-            _button("Touch"),
+            _button("Toast"),
           ],
         ),
       ],
@@ -62,10 +77,13 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  _img() {
-    return Image.asset(
-      "assets/images/dog1.png",
-      fit: BoxFit.cover,
+  _img(String img) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: Image.asset(
+        img,
+        fit: BoxFit.cover,
+      ),
     );
   }
 
