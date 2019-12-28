d129746ed03ca482446b5fcb144f33b0feeeaad4
Tharles Amaro <tharlesamaro@gmail.com>
2019-12-27 21:03:16 -0300

    Usando alinhamento dentro de um Stack

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -36,11 +36,14 @@ class HelloListView extends StatelessWidget {
           fit: StackFit.expand,
           children: <Widget>[
             _img(dog.foto),
-            Text(
-              dog.nome,
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.white,
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                dog.nome,
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.white,
+                ),
               ),
             ),
           ],
